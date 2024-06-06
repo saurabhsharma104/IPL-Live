c26b318401ec73dcf1a6baee3616725630b5fdd4
saurabh sharma <saurabhsharma29520@gmail.com>
2024-06-07 00:05:43 +0530

    [Fix] main page setup API

--- Backend/src/main/java/project/ipl_dashboard/controller/LiveMatchController.java
+++ Backend/src/main/java/project/ipl_dashboard/controller/LiveMatchController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import project.ipl_dashboard.config.APIConfig;
 
 @RestController
@@ -41,10 +45,6 @@ public class LiveMatchController {
     @GetMapping("/matchlist")
     public String getmatchesList() {
 
-        // if (matchList != null) {
-        //     return matchList;
-        // }
-
         try {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(APIConfig.BASE_URL + "/series/v1/"+APIConfig.SERIES_ID))
@@ -58,7 +58,44 @@ public class LiveMatchController {
             e.printStackTrace();
         } 
 
-        return matchList;
+        if (matchList == null) return matchList;
+
+         try {
+            // Convert JSON string to JSON object
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(matchList);
+
+            // Navigate to the node you want to update
+            JsonNode matchInfoNode = rootNode
+                .path("matchDetails")
+                .get(rootNode.path("matchDetails").size()-1)
+                .path("matchDetailsMap")
+                .path("match")
+                .get(0)
+                .path("matchInfo");
+
+            
+            if (matchInfoNode.isObject()) {
+                ObjectNode matchInfoObjectNode = (ObjectNode) matchInfoNode;
+                // Update the state
+                matchInfoObjectNode.put("state", "In Progress");
+            }
+            // System.out.println(matchInfoNode.toString());
+            
+
+            // Convert JSON object back to string
+            String updatedmatchList = objectMapper.writeValueAsString(rootNode);
+
+            // Print updated JSON string
+            // System.out.println(updatedmatchList);
+
+            return updatedmatchList;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
