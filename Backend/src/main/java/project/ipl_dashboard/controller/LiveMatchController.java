7809531c7bbe8a42947d6f6cf53da7fb4c3986dd
saurabh sharma <saurabhsharma29520@gmail.com>
2024-06-09 14:12:43 +0530

    [Build] Home page API

--- Backend/src/main/java/project/ipl_dashboard/controller/LiveMatchController.java
+++ Backend/src/main/java/project/ipl_dashboard/controller/LiveMatchController.java
@@ -10,10 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import project.ipl_dashboard.config.APIConfig;
 
 @RestController
@@ -57,45 +53,29 @@ public class LiveMatchController {
         } catch (Exception e) {
             e.printStackTrace();
         } 
+        return matchList;
+    }
 
-        if (matchList == null) return matchList;
 
-         try {
-            // Convert JSON string to JSON object
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(matchList);
+    @GetMapping("/preview-match-id")
+    public String getPreviewMatch() {
 
-            // Navigate to the node you want to update
-            JsonNode matchInfoNode = rootNode
-                .path("matchDetails")
-                .get(rootNode.path("matchDetails").size()-1)
-                .path("matchDetailsMap")
-                .path("match")
-                .get(0)
-                .path("matchInfo");
+        // if (matchList == null) {
+        //     try {
+        //         HttpRequest request = HttpRequest.newBuilder()
+        //                 .uri(URI.create(APIConfig.BASE_URL + "/series/v1/"+APIConfig.SERIES_ID))
+        //                 .header("X-RapidAPI-Key", APIConfig.API_KEY)
+        //                 .header("X-RapidAPI-Host", APIConfig.HOST_NAME)
+        //                 .method("GET", HttpRequest.BodyPublishers.noBody())
+        //                 .build();
+        //         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        //         matchList = response.body();
+        //     } catch (Exception e) {
+        //         e.printStackTrace();
+        //     } 
+        // }
 
-            
-            if (matchInfoNode.isObject()) {
-                ObjectNode matchInfoObjectNode = (ObjectNode) matchInfoNode;
-                // Update the state
-                matchInfoObjectNode.put("state", "In Progress");
-            }
-            // System.out.println(matchInfoNode.toString());
-            
-
-            // Convert JSON object back to string
-            String updatedmatchList = objectMapper.writeValueAsString(rootNode);
-
-            // Print updated JSON string
-            // System.out.println(updatedmatchList);
-
-            return updatedmatchList;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return "91740";
     }
 
 }
